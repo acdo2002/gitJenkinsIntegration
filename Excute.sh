acd0bed7281e1600d01ac4040b7dc892e5a5ab9c
Ming-yi Lin <acdo2002@gmail.com>
2021-11-26 16:46:34 +0800

    add prepare tests in Execute.sh

--- Excute.sh
+++ Excute.sh
@@ -11,11 +11,14 @@ echo ${CommitName}
 (cd /home/mingyi/jenkins-carta-backend-dev/${CommitName}/carta-backend/build ; sudo cmake ..)
 (cd /home/mingyi/jenkins-carta-backend-dev/${CommitName}/carta-backend/build ; sudo make -j 10)
 
-export PATH=$PATH:/home/mingyi/.nvm/versions/node/v12.18.1/bin/npm
 ## build carta performance regression tests
 (cd /home/mingyi/jenkins-carta-ICD-dev/ ; sudo mkdir ${CommitName})
 (cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName} ; sudo git clone --recursive https://github.com/CARTAvis/carta-backend-ICD-test.git)
 (cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName}/carta-backend-ICD-test ; sudo npm install)
 (cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName}/carta-backend-ICD-test/protobuf/ ; sudo ./build_proto.sh)
+
+## Prepare the tests
+(sudo cp /home/mingyi/carta-ICD-dev/codes/3fd6679/* /home/mingyi/jenkins-carta-ICD-dev/${CommitName}/carta-backend-ICD-test/src/test)
+(cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName}/carta-backend-ICD-test/src/test ; sudo sed -i -e 's/3fd6679/'${CommitName}'/g' export_ICD.sh)
 
 exit
