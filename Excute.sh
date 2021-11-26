5cbb4023421e533bbe48a3eb48b6ad96b5672634
Ming-yi Lin <acdo2002@gmail.com>
2021-11-26 15:47:33 +0800

    add build carta ICD performance regression tests

--- Excute.sh
+++ Excute.sh
@@ -4,10 +4,17 @@ name=`git log -1 --oneline`
 CommitName=$(echo $name | cut -d ' ' -f 1)
 echo ${CommitName}
 
+## build carta-backend
 (cd /home/mingyi/jenkins-carta-backend-dev/ ; sudo mkdir ${CommitName})
 (cd /home/mingyi/jenkins-carta-backend-dev/${CommitName} ; sudo git clone --recursive https://github.com/CARTAvis/carta-backend.git)
 (cd /home/mingyi/jenkins-carta-backend-dev/${CommitName}/carta-backend ; sudo mkdir build)
 (cd /home/mingyi/jenkins-carta-backend-dev/${CommitName}/carta-backend/build ; sudo cmake ..)
 (cd /home/mingyi/jenkins-carta-backend-dev/${CommitName}/carta-backend/build ; sudo make -j 10)
+
+## build carta performance regression tests
+(cd /home/mingyi/jenkins-carta-ICD-dev/ ; sudo mkdir ${CommitName})
+(cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName} ; sudo git clone --recursive https://github.com/CARTAvis/carta-backend-ICD-test.git)
+(cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName}/carta-backend-ICD-test ; sudo npm install)
+(cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName}/carta-backend-ICD-test/protobuf/ ; sudo ./build_proto.sh)
 
 exit
