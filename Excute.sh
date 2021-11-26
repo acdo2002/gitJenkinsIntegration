a85c197bc067dd2d16b657922fb8a9f09dff598f
Ming-yi Lin <acdo2002@gmail.com>
2021-11-26 15:55:47 +0800

    remove sudo npm install in Execute.h

--- Excute.sh
+++ Excute.sh
@@ -14,7 +14,7 @@ echo ${CommitName}
 ## build carta performance regression tests
 (cd /home/mingyi/jenkins-carta-ICD-dev/ ; sudo mkdir ${CommitName})
 (cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName} ; sudo git clone --recursive https://github.com/CARTAvis/carta-backend-ICD-test.git)
-(cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName}/carta-backend-ICD-test ; sudo npm install)
-(cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName}/carta-backend-ICD-test/protobuf/ ; sudo ./build_proto.sh)
+(cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName}/carta-backend-ICD-test ; npm install)
+(cd /home/mingyi/jenkins-carta-ICD-dev/${CommitName}/carta-backend-ICD-test/protobuf/ ; ./build_proto.sh)
 
 exit
