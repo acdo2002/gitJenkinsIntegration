81e80e5e8577f81bd2619b71bdb6e3e0a6c6d029
Ming-yi Lin <acdo2002@gmail.com>
2021-11-26 15:28:53 +0800

    create carta backend each commit in Execute.sh

--- Excute.sh
+++ Excute.sh
@@ -5,5 +5,9 @@ CommitName=$(echo $name | cut -d ' ' -f 1)
 echo ${CommitName}
 
 (cd /home/mingyi/jenkins-carta-backend-dev/ ; sudo mkdir ${CommitName})
+(cd /home/mingyi/jenkins-carta-backend-dev/${CommitName} ; sudo git clone --recursive https://github.com/CARTAvis/carta-backend.git)
+(cd /home/mingyi/jenkins-carta-backend-dev/${CommitName}/carta-backend ; sudo mkdir build)
+(cd /home/mingyi/jenkins-carta-backend-dev/${CommitName}/carta-backend/build ; sudo cmake ..)
+(cd /home/mingyi/jenkins-carta-backend-dev/${CommitName}/carta-backend/build ; sudo make -j 10)
 
 exit
